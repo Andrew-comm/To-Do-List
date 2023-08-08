76d6b771ed2273718ff8e8d937f3f75841e8c1e2
Andrew <ndrwrono2001@gmail.com>
2023-08-08 23:51:07 +0300

    added some styling

--- Task/Task/urls.py
+++ Task/Task/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('register', views.RegisterPage.as_view(), name='register'),
     path('logout', LogoutView.as_view(next_page = 'login'), name='logout'),
     path('', views.TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
+    # path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     path('task-create', views.TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', views.TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', views.TaskDelete.as_view(), name='task-delete'),
