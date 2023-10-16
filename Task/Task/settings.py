86ed6c477e62f00f70ceb6e3ec4f9007b27198f2
andrea <ndrwrono2001@gmail.com>
2023-10-16 18:05:19 +0300

    updated settings

--- Task/Task/settings.py
+++ Task/Task/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-effb_m#mo2idgi6t1cfxu!&!y(nf))*&3l-h=&*9mv7^2)q50%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
