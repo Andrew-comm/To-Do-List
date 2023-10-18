4d7034bdcb87481a0bd73211af36cde804e0f99e
andrea <ndrwrono2001@gmail.com>
2023-10-18 22:12:24 +0300

    conf for production

--- Task/Task/settings.py
+++ Task/Task/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('CUSTOM_SECTRET_KEY')) 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['*']#andreas-to-dos.onrender.com
+ALLOWED_HOSTS = ['andreas-to-dos.onrender.com']#andreas-to-dos.onrender.com
 
 
 # Application definition
